aae5e55a55ff7cfac109faac4fe434cccafa29ce
victorbruce <victorbruce82@gmail.com>
2023-06-24 08:12:18 +0000

    ✨ create a base layout for other tab bar veiw controllers

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -5,4 +5,12 @@
 //  Created by Victor Bruce on 19/06/2023.
 //
 
-import Foundation
+import UIKit
+
+class UpcomingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
--- Netflix Clone/MainTabBarViewController.swift
+++ Netflix Clone/MainTabBarViewController.swift
@@ -11,32 +11,26 @@ class MainTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemYellow
         
-        // embed each view controller in a UI navigation controller
-        let homeVc = UINavigationController(rootViewController: HomeViewController())
-        let searchVc = UINavigationController(rootViewController: SearchViewController())
-        let upcomingVc = UINavigationController(rootViewController: UpcomingViewController())
-        let downloadsVc = UINavigationController(rootViewController: DownloadsViewController())
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadViewController())
         
-        // add sf symbols to each tab bar item
-        homeVc.tabBarItem.image = UIImage(systemName: "house")
-        searchVc.tabBarItem.image = UIImage(systemName: "magnifyingglass")
-        upcomingVc.tabBarItem.image = UIImage(systemName: "play.circle")
-        downloadsVc.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
         
-        // add label to each tab bar item icon
-        homeVc.title = "Home"
-        searchVc.title = "Search"
-        upcomingVc.title = "Coming Soon"
-        downloadsVc.title = "Downloads"
+        vc1.title = "Home"
+        vc2.title = "Coming Soon"
+        vc3.title = "Top Search"
+        vc4.title = "Downloads"
         
-        // change tab bar label color
         tabBar.tintColor = .label
         
-        // set each UI navigation view controller to the UITabBarController
-        setViewControllers([homeVc,upcomingVc, searchVc, downloadsVc], animated: true)
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
-
 }
 
